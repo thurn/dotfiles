6544c176bef91ae98cbb960f2c643139a3ff0852
Derek Thurn <derek@thurn.ca>
2009-12-21 15:18:41 -0700

    Moved bin from .glob to home directory

--- .global/emacs/emacs.el
+++
@@ -1,8 +0,0 @@
-(global-set-key "\C-x\C-m" 'execute-extended-command)
-(global-set-key "\C-c\C-m" 'execute-extended-command)
-
-(global-set-key "\C-w" 'backward-kill-word)
-(global-set-key "\C-x\C-k" 'kill-region)
-(global-set-key "\C-c\C-k" 'kill-region)
-
-(defalias 'qrr 'query-replace-regexp)
