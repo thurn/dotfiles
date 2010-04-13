9f44298e4e51f197bf00b9a367067eba03cf4e63
Thor <thor@thurn.ca>
2010-04-13 10:26:29 -0400

    nXhtml mode added

---
+++ emacs/lisp/nxhtml_config.el
@@ -0,0 +1,6 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Configuration for nXhtml mode, the emacs web-development package.          ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(load (concat emacs-root-dthurn "site-lisp/nxhtml-2.05/autostart.el"))
+
