edfbddd337c0c7ccb84c3ce15dd868070c4767a4
Thor <thor@thurn.ca>
2010-04-14 10:15:29 -0400

    A few PF specific tweaks to js2-mode

--- emacs/lisp/js2_config.el
+++ emacs/lisp/js2_config.el
@@ -9,11 +9,11 @@
 ;; js2-mode configuration
 (setq js2-allow-keywords-as-property-names nil)
 (setq js2-auto-indent-p nil)
-(setq js2-basic-offset 2)
+(setq js2-basic-offset 4)
 (setq js2-bounce-indent-p t)
 (setq js2-cleanup-whitespace t)
 (setq js2-enter-indents-newline t)
-(setq js2-global-externs (quote ("algjs")))
+(setq js2-global-externs (quote ("algjs" "PrimalFusion")))
 (setq js2-highlight-level 3)
 (setq js2-idle-timer-delay 0.2)
 (setq js2-indent-on-enter-key t)
