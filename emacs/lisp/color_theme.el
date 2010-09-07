01bbbdda4a8b6336a56313730844ba6ac0a9f871
Derek Thurn <dthurn@dthurn-mbp.local>
2010-09-07 12:15:41 -0700

    Switched to the "zenburn" color theme.

--- emacs/lisp/color_theme.el
+++ emacs/lisp/color_theme.el
@@ -1,7 +1,12 @@
 ;; Configuration for color-theme, emacs syntax highlighting.
 
 (require 'color-theme)
+(require 'zenburn)
+
 (eval-after-load "color-theme"
   '(progn (color-theme-initialize)
-          (color-theme-hober)))
+          (color-theme-zenburn)))
+
+(unless (zenburn-format-spec-works-p)
+  (zenburn-define-format-spec))
 
