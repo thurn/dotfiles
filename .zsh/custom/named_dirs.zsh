d8c41e3dcc6f7957c4489ed66ae356f9449a6bc2
Derek Thurn <derek@thurn.ca>
2011-03-12 09:52:47 -0500

    Added the custom/ directory with my own customizations.

---
+++ .zsh/custom/named_dirs.zsh
@@ -0,0 +1,8 @@
+# Add yourself some shortcuts to projects you often work on
+# Example:
+#
+# brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
+#
+compiler=~/Dropbox/compiler/src
+tournica=~/Dropbox/tourni.ca
+ackbar=~/Dropbox/ackbar
--- .zshrc
+++ .zshrc
@@ -7,7 +7,7 @@ fi
 source ~/.dotfile_import
 
 # Path to your oh-my-zsh configuration.
-export ZSH=$HOME/.oh-my-zsh
+export ZSH=$HOME/.zsh
 
 # Set to the name theme to load.
 # Look in ~/.oh-my-zsh/themes/
