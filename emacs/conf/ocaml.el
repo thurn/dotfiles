6424a1825dc572e17bbaaff9748e607aeb42e77e
dthurn <dthurn@fb.com>
2011-06-27 23:28:33 -0700

    Minor ocaml-mode and sober changes

--- emacs/conf/ocaml.el
+++ emacs/conf/ocaml.el
@@ -4,3 +4,14 @@
 (autoload 'camldebug "camldebug" "Run the Caml debugger" t)
 (dolist (ext '(".cmo" ".cmx" ".cma" ".cmxa" ".cmi"))
   (add-to-list 'completion-ignored-extensions ext))
+(setq ocamlspot-command (executable-find "ocamlspot"))
+(autoload 'ocamlspot-query "ocamlspot" "OCamlSpot")
+
+(add-hook 'tuareg-mode-hook
+          '(lambda ()
+             (local-set-key "\C-c;" 'ocamlspot-query)
+             (local-set-key "\C-c\C-t" 'ocamlspot-type)
+             (local-set-key "\C-c\C-y" 'ocamlspot-type-and-copy)
+             (local-set-key "\C-c\C-u" 'ocamlspot-use)
+             (local-set-key "\C-ct" 'caml-types-show-type)))
+
