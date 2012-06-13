98e0b8b772e52393b7d1529ec0009c778a68a655
Derek Thurn <dthurn@Terezas-MacBook-Air.local>
2012-06-12 21:18:15 -0700

    hask

--- emacs/conf/haskell.el
+++ emacs/conf/haskell.el
@@ -2,7 +2,7 @@
 
 (require 'hs-lint)
 
-(load "~/emacs/lib/haskell-mode-2.8.0/haskell-site-file.el")
+(load "~/emacs/lib/haskell-mode/haskell-site-file.el")
 
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
