16f2a1acde122ef8314f4be64ad94fd8083a247b
Derek Thurn <derek@fake.email>
2013-06-01 17:45:44 -0700

    starred

--- emacs/conf/sober.el
+++ emacs/conf/sober.el
@@ -107,7 +107,7 @@ region) apply comment-or-uncomment to the current line"
            "*grep*" "*Compile-Log*" "*Shell Command Output*" "*compilation*"
            "*Occur*" "*log*" "*epic output*" "*git-status*"
            "*Async Shell Command*" "*save*" "*piped*" "*Async Shell Command Output*"
-           "*cs*" "*Ack-and-a-half*"
+           "*cs*" "*Ack-and-a-half*" "*coffee-compiled*"
            )))
     (mapcar 'kill-buffer-if-exists buffers)))
 
